5214a0e731f87b5f1f15b029b7b24dbc42861173
Toshio Morita <morita@sowa.com>
2022-06-21 22:53:25 +0900

    UD CorpusのUPOS,XPOSの簡単なチェッカープログラム（ハイフンの有無をチェック）追加

---
+++ tools/ConvertUDCorpus/CheckPOS/Program.cs
@@ -0,0 +1,45 @@
+﻿var subdirs = Directory.GetDirectories(args[0], "UD_*");
+foreach (var subdir in subdirs)
+{
+    var subdirname = Path.GetFileName(subdir);
+    var tokens = subdirname.Substring(3).Split('-');
+    if (tokens.Length != 2)
+    {
+        throw new Exception($"Invalid folder name format: {subdir}");
+    }
+    var files = Directory.GetFiles(subdir, "*.conllu");
+    if (files.Length == 0)
+    {
+        throw new Exception($"CONLLU file not found in {subdir}.");
+    }
+
+    foreach (var file in files)
+    {
+        var found = false;
+        using (var rdr = new StreamReader(file))
+        {
+            while (!rdr.EndOfStream)
+            {
+                var line = rdr.ReadLine();
+                var tkns = line.Split('\t');
+                if (tkns.Length == 10)
+                {
+                    var upos = tkns[3];
+                    var xpos = tkns[4];
+                    if (upos != "_" && upos.IndexOf('-') >= 0)
+                    {
+                        found = true;
+                        Console.WriteLine($"{file} upos={upos}");
+                        break;
+                    }
+                    if (xpos != "_" && xpos.IndexOf('-') >= 0)
+                    {
+                        //found = true;
+                        //Console.WriteLine($"{file} xpos={xpos}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
